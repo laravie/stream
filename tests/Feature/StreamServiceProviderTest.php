49323a8944b60e2e16a91bf329e13efda3ad2707
Mior Muhammad Zaki <crynobone@gmail.com>
2019-04-17 12:34:36 +0800

    Initate services.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Feature/StreamServiceProviderTest.php
+++ tests/Feature/StreamServiceProviderTest.php
@@ -3,6 +3,7 @@
 namespace Laravie\Stream\Tests\Feature;
 
 use React\EventLoop\LoopInterface;
+use React\Stream\WritableResourceStream;
 use React\Stream\WritableStreamInterface;
 
 class StreamServiceProviderTest extends TestCase
@@ -12,5 +13,8 @@ class StreamServiceProviderTest extends TestCase
     {
         $this->assertTrue($this->app->bound(LoopInterface::class));
         $this->assertTrue($this->app->bound(WritableStreamInterface::class));
+
+        $this->assertInstanceOf(LoopInterface::class, $this->app->make(LoopInterface::class));
+        $this->assertInstanceOf(WritableResourceStream::class, $this->app->make(WritableStreamInterface::class));
     }
 }
